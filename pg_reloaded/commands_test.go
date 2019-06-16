ab3449382368e9b3aec918761d566f7f3ff68793
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2019-06-16 04:04:50 +0200

    Fix test cases

--- pg_reloaded/commands_test.go
+++ pg_reloaded/commands_test.go
@@ -4,20 +4,52 @@ import (
 	"testing"
 )
 
+func TestCreateDatabaseArgs(t *testing.T) {
+	want := []string{
+		"-U", "user",
+		"-h", "my-host",
+		"-p", "5432",
+		"-d", "postgres",
+		"-c", "CREATE DATABASE test_database OWNER user",
+	}
+	have := createDatabaseArgs("user", "test_database", "my-host", 5432)
+
+	for idx, val := range want {
+		if have[idx] != val {
+			t.Errorf("TestCreateDatabaseArgs want: %s have:%s", val, have[idx])
+		}
+	}
+}
+
 func TestDropDatabaseArgs(t *testing.T) {
-	want := "-U \"user\" -d \"postgres\" -h \"my-host\" -p 5432 -c \"DROP DATABASE test_database\""
+	want := []string{
+		"-U", "user",
+		"-h", "my-host",
+		"-p", "5432",
+		"-d", "postgres",
+		"-c", "DROP DATABASE test_database",
+	}
 	have := dropDatabaseArgs("user", "test_database", "my-host", 5432)
 
-	if want != have {
-		t.Errorf("TestDropDatabaseArgs want: %s have:%s", want, have)
+	for idx, val := range want {
+		if have[idx] != val {
+			t.Errorf("TestDropDatabaseArgs want: %s have:%s", val, have[idx])
+		}
 	}
 }
 
 func TestPsqlArgs(t *testing.T) {
-	want := "-U \"user\" -d \"test_database\" -h \"my-host\" -p 5432"
+	want := []string{
+		"-U", "user",
+		"-h", "my-host",
+		"-p", "5432",
+		"-d", "test_database",
+	}
 	have := psqlArgs("user", "test_database", "my-host", 5432)
-
-	if want != have {
-		t.Errorf("TestPsqlArgs want: %s have:%s", want, have)
+	
+	for idx, val := range want {
+		if have[idx] != val {
+			t.Errorf("TestPsqlArgs want: %s have:%s", val, have[idx])
+		}
 	}
 }
